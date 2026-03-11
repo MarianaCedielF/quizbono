5aac32dd1054098cd76640ac83e3c2e8dd99b6af
MarianaCedielF <m.cediel1@uniandes.edu.co>
2026-03-11 10:27:33 -0500

    global, types, page ppal

---
+++ lib/types.ts
@@ -0,0 +1,16 @@
+export type Property = {
+  id: number;
+  propertyName: string;
+  city: string;
+  price: string;
+  garage: boolean;
+  rooms: number;
+  baths: number;
+  latitute: number;
+  longitude: number;
+  fullName: string;
+  address: string;
+  phone: string;
+  email: string;
+  floor: number;
+};
